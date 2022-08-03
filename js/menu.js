03f2e60556a624477cb338d290b079135bb0220c
Nate Crawford <nathanwcrawford22@gmail.com>
2022-08-03 13:19:53 -0400

    Another Commit

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+function dropDown() {
+    document.getElementById("dropMenu").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdownContent");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
